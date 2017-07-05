19cca47d9daf38904927ada222fb0a31c6d7b4c4
Daniel Moniz <daniel.moniz@gmail.com>
2017-07-05 11:29:24 -0400

    Add more similar closure exercises.

--- closure_exercises.js
+++ closure_exercises.js
@@ -18,11 +18,14 @@ helloWorld(); // Hello world!
 
 
 
+
+
+
 // Closure example that involves an argument
 var number = 3;
+var printNumber = getPrintNumberFunction(number);
 
 // FILL THIS IN --------------------------
-var printNumber = getPrintNumberFunction(number);
 
 function getPrintNumberFunction(number) {
   return function() {
@@ -34,6 +37,35 @@ function getPrintNumberFunction(number) {
 printNumber(); // 3
 number = 5;
 printNumber(); // 3
+
+
+
+
+
+
+// Another closure example that involves an argument
+var movie = "The Matrix";
+// Note that you will have to
+
+// FILL THIS IN --------------------------
+var tellFavouriteMovie = getFavouriteMovie(movie);
+
+function getFavouriteMovie(movieName) {
+  return function() {
+    console.log(movieName);
+  }
+}
+// ---------------------------------------
+
+tellFavouriteMovie(); // The Matrix
+movie = "Primer";
+tellFavouriteMovie(); // The Matrix
+
+
+
+
+
+
 
 
 // Multiple arguments
@@ -52,16 +84,21 @@ function getUsualOrderFunction(sandwiches, burgers) {
 
 theUsual(); // We'll get 2 sandwiches and 3 burgers.
 sandwiches = 0;
-burgers = 3;
+burgers = 1;
 theUsual(); // We'll get 2 sandwiches and 3 burgers.
+
+
+
+
+
 
 
 
 // Generate a function that requires arguments
 
 var recipient = "Devon";
-// FILL THIS IN --------------------------
 var sendNote = getSendNoteFunction(recipient);
+// FILL THIS IN --------------------------
 
 function getSendNoteFunction(recipient) {
   return function(message) {
@@ -77,6 +114,38 @@ recipient = "Mark";
 sendNote("You are cordially invited...");
 // Dear Devon,
 // You are cordially invited...
+
+
+
+
+
+
+
+
+// Generate a function that requires arguments
+
+var personToVisit = "best friend";
+// FILL THIS IN --------------------------
+var flyTo = getFlyToCityFunction(personToVisit);
+
+function getFlyToCityFunction(person) {
+  return function(targetCity) {
+    console.log("Enjoy your visit to " + targetCity + " to visit your " + person + ".");
+    // console.log("Dear " + personToVisit + ",\n" + city);
+  }
+}
+// ---------------------------------------
+
+flyTo("Shanghai");
+// Enjoy your visit to Shanghai to visit your best friend.
+personToVisit = "Shanghai";
+flyTo("Lagos");
+// Enjoy your visit to Lagos to visit your best friend.
+
+
+
+
+
 
 
 
